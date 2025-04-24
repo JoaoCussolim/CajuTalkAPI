c43293cba71d45cc32cb96e19442da877a370b16
MatheusFranco2008 <162757230+MatheusFranco2008@users.noreply.github.com>
2025-04-24 10:18:48 -0300

    ajustes

--- Data/CajuTalkContext.cs
+++ Data/CajuTalkContext.cs
@@ -8,14 +8,14 @@ namespace TWTodos.Data
         public CajuTalkContext(DbContextOptions<CajuTalkContext> options)
             : base(options) { }
 
-        public DbSet<Usuario> Usuarios { get; set; }            // cria a tabela Usuarios
-        public DbSet<SalaChat> SalasChat { get; set; }          // cria a tabela SalasChat
-        public DbSet<UsuarioSala> UsuariosSala { get; set; }    // cria a tabela UsuariosSala
-        public DbSet<Mensagem> Mensagens { get; set; }          // cria a tabela Mensagens
+        public DbSet<Usuario> Usuarios { get; set; }            // cria a variavel que representa a tabela Usuarios 
+        public DbSet<SalaChat> SalasChat { get; set; }          // cria a variavel que representa a tabela SalasChat
+        public DbSet<UsuarioSala> UsuariosSala { get; set; }    // cria a variavel que representa a tabela UsuariosSala
+        public DbSet<Mensagem> Mensagens { get; set; }          // cria a variavel que representa a tabela Mensagens
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.HasDefaultSchema("CajuTalk");          // cria o schema cajutalk
+            modelBuilder.HasDefaultSchema("CajuTalk");          // define o schema cajutalk
 
             modelBuilder.Entity<UsuarioSala>()
                 .HasIndex(us => new { us.ID_Usuario, us.ID_Sala })
